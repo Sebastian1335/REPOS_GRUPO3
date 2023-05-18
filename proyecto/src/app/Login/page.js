1ca79a16361f96ae8e0c9c949fde04520147901e
SEBASTIAN GUZMAN SOTO <20200981@aloe.ulima.edu.pe>
2023-05-17 23:58:45 -0500

    Pantalla login (sin funcionalidad)

---
+++ proyecto/src/app/Login/page.js
@@ -0,0 +1,63 @@
+'use client';
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Stack from 'react-bootstrap/Stack';
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import styles from './styles.css'
+
+const Login = () => {
+
+    const [usuario, setUsuario] = useState('')
+    const [password, setPassword] = useState('')
+
+    const router = useRouter()
+
+    const handleClick = () => {
+        if (usuario==="admin" && password==="admin") {
+            router.push('/Login')
+        } else
+            alert('Usuario o password incorrecto')
+    }
+
+    return (
+        <div className='container'>
+            <div>
+                <h3>Sistema de citas para Atencion a Estudiantes</h3>
+            </div>
+        <Form className='form'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Coreo o Usuario</Form.Label>
+                <Form.Control type="email" placeholder="" />
+                
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="" />
+                <Form.Text className="text-muted" >
+                    <button type="button" class="btn btn-link"><p className='letra'>Registro de nuevo usuario </p> </button>
+                    <button type="button" class="btn btn-link"> <p className='letra'> olvide mi contraseña </p></button> 
+                </Form.Text>
+            </Form.Group>
+            <div className='iz'>
+                
+                <Button variant="primary" type="button" class="btn btn-outline-primary">
+                    Salir
+                </Button>
+                <div className='spc'>
+                
+                <Button variant="primary" type="submit">
+                    Ingresar
+                </Button>
+                </div>
+            </div>
+    </Form>
+    </div>
+    )
+} 
+
+export default Login
