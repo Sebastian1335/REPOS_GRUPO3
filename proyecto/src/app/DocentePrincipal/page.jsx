0d57f169aff15dc010193b86e21b81bea336bb9c
GiaCarrion <gacame1501@gmail.com>
2023-05-26 15:24:09 -0500

    restriccion de redireccion

--- proyecto/src/app/DocentePrincipal/page.jsx
+++ proyecto/src/app/DocentePrincipal/page.jsx
@@ -1,54 +1,53 @@
-'use client';
-import React, { useState, useEffect} from 'react';
-import { useRouter } from 'next/navigation';
-import TopBar from '@/components/TopBar/TopBar';
-import Menu from '@/components/MenuDocente/Menu';
-import styles from '../DocentePrincipal/page.module.css'
-
+"use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import TopBar from "@/components/TopBar/TopBar";
+import Menu from "@/components/MenuDocente/Menu";
+import styles from "../DocentePrincipal/page.module.css";
+import PrivateRoute from "@/components/PrivateRoute/PrivateRoute";
 
 const Docente = () => {
-    //Aparecer nombre en la pantalla
-    const router = useRouter();
-    
-    const [nombreUsuario, setNombreUsuario] = useState('');
+  //Aparecer nombre en la pantalla
+  const router = useRouter();
 
-    useEffect(() => {
-        const nombreUsuario = localStorage.getItem('nombreUsuario');
-        setNombreUsuario(nombreUsuario);
-    }, []);
-    
-    //Aparecer Menu
-    const [menuIsVisible, setMenuIsVisible] = useState(false);
-    
-    function aparecerMenu()
-    {
-        setMenuIsVisible(!menuIsVisible);
-    }
+  const [nombreUsuario, setNombreUsuario] = useState("");
 
-    let barraLateral;
+  useEffect(() => {
+    const nombreUsuario = localStorage.getItem("nombreUsuario");
+    setNombreUsuario(nombreUsuario);
+  }, []);
 
-    if (menuIsVisible){
-        barraLateral = <Menu/>
-    }
-    
-    return (
-        <div>
-            <TopBar onButtonClick={aparecerMenu}></TopBar>
-            <div className={styles.Main}>
-                <div className={styles.Info}>
-                    <h2>Bienvenido, Profesor(a) {nombreUsuario}!</h2>
-                    <hr/>  
-                    <div className={styles.Fondo}>
-                        <h2>Próximas Citas</h2>
-                    </div>
-                </div>
+  //Aparecer Menu
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
 
-                <div className={styles.Menu}>
-                    {barraLateral}
-                </div>
+  function aparecerMenu() {
+    setMenuIsVisible(!menuIsVisible);
+  }
+
+  let barraLateral;
+
+  if (menuIsVisible) {
+    barraLateral = <Menu />;
+  }
+
+  return (
+    <PrivateRoute rolesPermitidos={["profesor"]}>
+      <div>
+        <TopBar onButtonClick={aparecerMenu}></TopBar>
+        <div className={styles.Main}>
+          <div className={styles.Info}>
+            <h2>Bienvenido, Profesor(a) {nombreUsuario}!</h2>
+            <hr />
+            <div className={styles.Fondo}>
+              <h2>Próximas Citas</h2>
             </div>
-        </div>
-    )
-} 
+          </div>
 
-export default Docente
+          <div className={styles.Menu}>{barraLateral}</div>
+        </div>
+      </div>
+    </PrivateRoute>
+  );
+};
+
+export default Docente;
