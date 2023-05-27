5b07cd32642f5b5fb562b54e6d87ce1631d64fe5
SEBASTIAN GUZMAN SOTO <20200981@aloe.ulima.edu.pe>
2023-05-27 00:44:05 -0500

    otras cositas a DarCalif

--- proyecto/src/app/DarCalificacion/page.js
+++ proyecto/src/app/DarCalificacion/page.js
@@ -1,23 +1,50 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import './style.css'; // Importa el archivo de estilos personalizados
 const Calificacion = () => {
     const [calificacion, setCalificacion] = useState(0);
-  
+    const [nombreUsuario, setNombreUsuario] = useState("");
+    const [apellidoUsuario, setApellidoUsuario] = useState("");
+    const [comentario, setComentario] = useState('');
+
     const handleCalificacionChange = (event) => {
       const value = parseFloat(event.target.value);
       setCalificacion(value);
     };
-  
+    
+    const defaultCalificacion = {
+        nombres: "",
+        apellidos: "",
+        estrellas: 0.0,
+        descripcion: ""
+    }
+
+
+
     const handleSubmit = (event) => {
       event.preventDefault();
       console.log(`Calificación seleccionada: ${calificacion} estrellas`);
+      console.log(`Comentario: ${comentario}`);
+
       // Aquí puedes agregar la lógica para enviar la calificación al servidor o realizar otras acciones necesarias
     };
-  
+    const handleComentarioChange = (event) => {
+        const value = event.target.value;
+        setComentario(value);
+      };
+
+
+    useEffect(() => {
+        const nombreUsuario = localStorage.getItem("nombreUsuario");
+        const apellidoUsuario = localStorage.getItem("apellidoUsuario")
+        setNombreUsuario(nombreUsuario);
+        setApellidoUsuario(apellidoUsuario)
+      }, []);
+
     return (
       <div className="calificacion-container">
+        <h2>{nombreUsuario} {apellidoUsuario}</h2>
         <h2 className="calificacion-title">Calificar</h2>
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="calificacion">
@@ -32,6 +59,15 @@ const Calificacion = () => {
             />
             <span className="calificacion-value">{calificacion}</span>
           </Form.Group>
+          <Form.Group controlId="comentario">
+          <Form.Label>Comentario:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={comentario}
+            onChange={handleComentarioChange}
+          />
+        </Form.Group>
           <Button variant="primary" type="submit" className="submit-button">
             Enviar
           </Button>
