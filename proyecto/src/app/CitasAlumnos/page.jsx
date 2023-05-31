d019e4745924f9e1271dfde85374c891ad1146ad
Sebastian Torres <sebastiantb@outlook.com>
2023-05-31 17:06:36 -0500

    Citas pasadas y futuras

--- proyecto/src/app/CitasAlumnos/page.jsx
+++ proyecto/src/app/CitasAlumnos/page.jsx
@@ -14,7 +14,6 @@ const CitaAlumno = () => {
   //Mostrar Menu
   const [menuIsVisible, setMenuIsVisible] = useState(false);
   const [buttonText, setButtonText] = useState("Ver citas futuras");
-  const [estrellas, setEstrellas] = useState(0);
 
   function aparecerMenu() {
     setMenuIsVisible(!menuIsVisible);
@@ -23,7 +22,6 @@ const CitaAlumno = () => {
   function handleClick() {
     if (buttonText == "Ver citas pasadas") {
       setButtonText("Ver citas futuras");
-      return;
     } else {
       setButtonText("Ver citas pasadas");
     }
@@ -35,13 +33,58 @@ const CitaAlumno = () => {
     barraLateral = <Menu />;
   }
 
+  useEffect(() => {
+    obtenerCitas();
+  });
+
+  // Usado en este componente
+
   const [show, setShow] = useState(false);
+  const [estrellas, setEstrellas] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const ratingChanged = (newRating) => {
-    setEstrellas(newRating);
+    setEstrellas([...estrellas, newRating]);
+  };
+
+  // Obtener citas pasadas o futuras
+  const obtenerDiferenciaDias = (date) => {
+    let fechaHoy = new Date();
+
+    let fecha = new Date(date);
+
+    const diferenciaDias = Math.floor(
+      (fechaHoy - fecha) / (1000 * 60 * 60 * 24)
+    );
+
+    return diferenciaDias;
+  };
+
+  // Obtener citas pasadas y futuras
+  const [citasPasadas, setCitasPasadas] = useState([]);
+  const [citasFuturas, setCitasFuturas] = useState([]);
+
+  const obtenerCitas = () => {
+    const citasFut = [];
+    const citasPas = [];
+    Profesores.forEach((profesor) => {
+      const diferenciaDias = obtenerDiferenciaDias(profesor.dia);
+      if (diferenciaDias > 0) {
+        citasFut.push(profesor);
+      } else {
+        citasPas.push(profesor);
+      }
+    });
+    setCitasFuturas(citasFut);
+    setCitasPasadas(citasPas);
+  };
+
+  // Transformar formato de fecha dd/mm/yyyy a texto
+  const transformarFechas = (fecha) => {
+    let opciones = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(fecha).toLocaleDateString([], opciones);
   };
 
   return (
@@ -78,132 +121,156 @@ const CitaAlumno = () => {
                     </Button>
                   </Col>
                 </Row>
-                {buttonText === "Ver citas pasadas" ? (
-                  <Row>
-                    {Profesores.map((profesor) => (
-                      <Col className="col-3 me-3">
-                        <div className={styles.container}>
-                          <Row>
-                            <Col className={styles.subcontainer}>
-                              <div className={styles.icono}></div>
-                              <div className="mb-3">
-                                <div className={styles.cardTitle}>
-                                  {profesor.nombreCompleto}
+                <Row>
+                  {buttonText === "Ver citas pasadas"
+                    ? citasPasadas.map((profesor) => (
+                        <Col
+                          className="col-3 me-3"
+                          key={profesor.nombreCompleto}
+                        >
+                          <div className={styles.container}>
+                            <Row>
+                              <Col className={styles.subcontainer}>
+                                <div className={styles.icono}>
+                                  <span>
+                                    {profesor.nombreCompleto.substring(0, 1)}
+                                  </span>
+                                  <span>
+                                    {profesor.nombreCompleto.substring(
+                                      profesor.nombreCompleto.indexOf(" "),
+                                      profesor.nombreCompleto.indexOf(" ") + 2
+                                    )}
+                                  </span>
                                 </div>
-                                <div className={styles.cardSubtitle}>
-                                  {profesor.profesion}
+                                <div className="mb-3">
+                                  <div className={styles.cardTitle}>
+                                    {profesor.nombreCompleto}
+                                  </div>
+                                  <div className={styles.cardSubtitle}>
+                                    {profesor.profesion}
+                                  </div>
                                 </div>
-                              </div>
-                            </Col>
-                          </Row>
-                          <Row className={styles.imagen}>
-                            <Col>
-                              <div></div>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.horario}>
-                              {profesor.dia} - {profesor.horarioInicio}
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.curso}>
-                              Curso: {profesor.curso}
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.zoom}>Enlace de Zoom</Col>
-                          </Row>
-                          <Row>
-                            <Col className="text-end">
-                              <Button className={styles.innerButton}>
-                                <span className={styles.innerTxt}>
-                                  Cancelar
+                              </Col>
+                            </Row>
+                            <Row className={styles.imagen}>
+                              <Col>
+                                <div></div>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.horario}>
+                                {transformarFechas(profesor.dia)} -{" "}
+                                {profesor.horarioInicio}
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.curso}>
+                                Curso: {profesor.curso}
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.zoom}>Enlace de Zoom</Col>
+                            </Row>
+                            <Row className="flex-grow-1">
+                              <Col className="d-flex align-items-end justify-content-end">
+                                <Button className={styles.innerButton}>
+                                  <span className={styles.innerTxt}>
+                                    Cancelar
+                                  </span>
+                                </Button>
+                              </Col>
+                            </Row>
+                          </div>
+                        </Col>
+                      ))
+                    : citasFuturas.map((profesor) => (
+                        <Col
+                          className="col-3 me-3"
+                          key={profesor.nombreCompleto}
+                        >
+                          <div className={styles.container}>
+                            <Row>
+                              <Col className={styles.subcontainer}>
+                                <div className={styles.icono}>
+                                  <span>
+                                    {profesor.nombreCompleto.substring(0, 1)}
+                                  </span>
+                                  <span>
+                                    {profesor.nombreCompleto.substring(
+                                      profesor.nombreCompleto.indexOf(" "),
+                                      profesor.nombreCompleto.indexOf(" ") + 2
+                                    )}
+                                  </span>
+                                </div>
+                                <div className="mb-3">
+                                  <div className={styles.cardTitle}>
+                                    {profesor.nombreCompleto}
+                                  </div>
+                                  <div className={styles.cardSubtitle}>
+                                    {profesor.profesion}
+                                  </div>
+                                </div>
+                              </Col>
+                            </Row>
+                            <Row className={styles.imagen}>
+                              <Col>
+                                <div></div>
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.horario}>
+                                {transformarFechas(profesor.dia)} -{" "}
+                                {profesor.horarioInicio}
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.curso}>
+                                Curso: {profesor.curso}
+                              </Col>
+                            </Row>
+                            <Row>
+                              <Col className={styles.zoom}>Enlace de Zoom</Col>
+                            </Row>
+                            <Row className="flex-grow-1 ">
+                              <Col className="d-flex align-items-center justify-content-end">
+                                <span className={styles.nota}>
+                                  {estrellas == 0
+                                    ? "No calificado"
+                                    : "Calificación: " + estrellas}
                                 </span>
-                              </Button>
-                            </Col>
-                          </Row>
-                        </div>
-                      </Col>
-                    ))}
-                  </Row>
-                ) : (
-                  <Row>
-                    {Profesores.map((profesor) => (
-                      <Col className="col-3 me-3">
-                        <div className={styles.container}>
-                          <Row>
-                            <Col className={styles.subcontainer}>
-                              <div className={styles.icono}></div>
-                              <div className="mb-3">
-                                <div className={styles.cardTitle}>
-                                  {profesor.nombreCompleto}
-                                </div>
-                                <div className={styles.cardSubtitle}>
-                                  {profesor.profesion}
-                                </div>
-                              </div>
-                            </Col>
-                          </Row>
-                          <Row className={styles.imagen}>
-                            <Col>
-                              <div></div>
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.horario}>
-                              {profesor.dia} - {profesor.horarioInicio}
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.curso}>
-                              Curso: {profesor.curso}
-                            </Col>
-                          </Row>
-                          <Row>
-                            <Col className={styles.zoom}>Enlace de Zoom</Col>
-                          </Row>
-                          <Row>
-                            <Col className="d-flex justify-content-end align-items-end mb-3">
-                              <span className={styles.nota}>
-                                {estrellas == 0
-                                  ? "No calificado"
-                                  : "Calificación: " + estrellas}
-                              </span>
-                            </Col>
-                            <Col className="text-end">
-                              <Button className={styles.innerButton}>
-                                <span
-                                  className={styles.innerTxt}
-                                  onClick={handleShow}
-                                >
-                                  Calificar
-                                </span>
-                              </Button>
-                            </Col>
-                          </Row>
-                          {/* DIALOG */}
-                          <Modal show={show} onHide={handleClose}>
-                            <Modal.Header closeButton>
-                              <Modal.Title>Califica la atención</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                              Es muy importante para nosotros saber cómo te fue
-                              en tu sesión de asesoría.
-                              <ReactStars
-                                count={5}
-                                size={24}
-                                value={estrellas}
-                                onChange={ratingChanged}
-                                color2={"#ffd700"}
-                              />
-                            </Modal.Body>
-                          </Modal>
-                        </div>
-                      </Col>
-                    ))}
-                  </Row>
-                )}
+                              </Col>
+                              <Col className="d-flex align-items-end justify-content-end">
+                                <Button className={styles.innerButton}>
+                                  <span
+                                    className={styles.innerTxt}
+                                    onClick={handleShow}
+                                  >
+                                    Calificar
+                                  </span>
+                                </Button>
+                              </Col>
+                            </Row>
+                            {/* DIALOG */}
+                            <Modal show={show} onHide={handleClose}>
+                              <Modal.Header closeButton>
+                                <Modal.Title>Califica la atención</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body>
+                                Es muy importante para nosotros saber cómo te
+                                fue en tu sesión de asesoría.
+                                <ReactStars
+                                  count={5}
+                                  size={24}
+                                  value={estrellas}
+                                  onChange={ratingChanged}
+                                  color2={"#ffd700"}
+                                />
+                              </Modal.Body>
+                            </Modal>
+                          </div>
+                        </Col>
+                      ))}
+                </Row>
               </div>
             ) : (
               <div>
@@ -238,7 +305,7 @@ const Profesores = [
     nombreCompleto: "Juan Lopez",
     profesion: "Mg. Ingeniería de Sistemas",
     imagen: require("../../../public/images/Profesor/profesor.png"),
-    dia: "Lunes, 24 de abril de 2023",
+    dia: "01/24/2023",
     horarioInicio: "08:00 am",
     curso: "Programación Web",
   },
@@ -246,8 +313,24 @@ const Profesores = [
     nombreCompleto: "Adriana Sanchez",
     profesion: "Mg. Ingeniería de Sistemas",
     imagen: require("../../../public/images/Profesor/profesora.png"),
-    dia: "Jueves, 27 de abril de 2023",
+    dia: "02/27/2023",
     horarioInicio: "04:00 pm",
+    curso: "Programación orientada a objetos",
+  },
+  {
+    nombreCompleto: "Herbert Huaman",
+    profesion: "Mg. Ingeniería de Sistemas",
+    imagen: require("../../../public/images/Profesor/profesor.png"),
+    dia: "06/23/2023",
+    horarioInicio: "10:00 am",
+    curso: "Programación Web",
+  },
+  {
+    nombreCompleto: "Marco Antonio",
+    profesion: "Mg. Ingeniería de Sistemas",
+    imagen: require("../../../public/images/Profesor/profesora.png"),
+    dia: "06/30/2023",
+    horarioInicio: "07:00 pm",
     curso: "Programación orientada a objetos",
   },
 ];
