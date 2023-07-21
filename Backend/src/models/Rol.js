15e99e459065e140ca8171d092aacbae13c17671
GiaCarrion <gacame1501@gmail.com>
2023-07-21 17:10:08 -0500

    ayuda

--- Backend/src/models/Rol.js
+++ Backend/src/models/Rol.js
@@ -1,9 +1,6 @@
 import { DataTypes } from 'sequelize'
 import sequelize from '../config/database.js'
 
-import { DataTypes } from 'sequelize'
-import sequelize from '../config/database.js'
-
 const Rol = sequelize.define('Rol', {
     idRol: {
         type:DataTypes.INTEGER,
