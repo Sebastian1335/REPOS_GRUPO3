ac325a8059763f2aad992a15368bc18e70a662d5
GiaCarrion <gacame1501@gmail.com>
2023-07-21 20:23:03 -0500

    xd

--- Backend/src/routes/personaCurso.js
+++ Backend/src/routes/personaCurso.js
@@ -7,11 +7,6 @@ const router = Express.Router()
 router.get("/", findAll)
 router.get("/id:", findOne)
 router.post("/", create)
-<<<<<<< HEAD
-router.put("", update)
-router.delete("/:id", remove)
-=======
 router.put("/", update)
->>>>>>> 4478ca3c8dbdfd6fd6d5bef1471f9a8395e6ff58
 
 export default router
