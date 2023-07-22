eca3e63e5e75def4f8df4672ef7f498c3fcccb6d
AdriUlima <20200711@aloe.ulima.edu.pe>
2023-07-22 03:45:18 -0500

    antes de siesta

--- Frontend/src/app/Admin/page.jsx
+++ Frontend/src/app/Admin/page.jsx
@@ -4,16 +4,22 @@ import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import Table from 'react-bootstrap/Table'
 import { useState, useEffect } from 'react'
 import personaApi from '../api/persona.js'
+import rolApi from '../api/rol.js'
 
 export default function Admin() {
    
   const [personas, setPersonas] = useState([])
+  const [roles, setRoles] = useState([])
   const [bus, setBus] = useState('')
   const [busq, setBusq] = useState('')
 
   const handleOnLoad = async() => {
     const result = await personaApi.findAll()
     setPersonas(result.data)
+    const result2 = await rolApi.findAll()
+    setRoles(result2.data)
+    console.log(roles)
+    console.log(roles.find(i => i.idRol == 1))
   }
 
   const handleBuscar = () => {
@@ -51,10 +57,8 @@ export default function Admin() {
                       <thead>
                         <tr>
                           <th scope="col">ID</th>
-                          <th scope="col">Nombre</th>
-                          <th scope="col">Apellido</th>
-                          <th scope="col">Tipo Documento</th>
-                          <th scope="col">Número</th>
+                          <th scope="col">Nombre completo</th>
+                          <th scope="col">Documento</th>
                           <th scope="col">Rol</th>
                           <th scope="col">Email</th>
                           <th scope="col">Contraseña</th>
@@ -63,16 +67,14 @@ export default function Admin() {
                       </thead>
                       <tbody>
                         { personas?.filter(item => (item.nombre.toLowerCase().includes(busq.toLowerCase()))).map(item => (
-                          <tr key={item.id}>
+                          <tr key={item.idPersona}>
                             <th scope="row" >
-                              <a href={'mantenimiento/'+item.id}>
-                                {item.id}
+                              <a href={'mantenimiento/'+item.idPersona}>
+                                {item.idPersona}
                               </a>
                               </th>
-                            <td>{item.nombre}</td>
-                            <td>{item.apellido}</td>
-                            <td>{item.tipoDocumento}</td>
-                            <td>{item.dni}</td>
+                            <td>{item.nombre} {item.apellido}</td>
+                            <td>{item.tipoDocumento}: {item.dni}</td>
                             <td>{item.idRol}</td>
                             <td>{item.email}</td>
                             <td>{item.contrasena}</td>
